c718cc7d3edf0d074a23e1786be8f9dc658e9502
Alexander Alemayhu <alexander@alemayhu.com>
2016-11-05 16:05:55 +0100

    staging: update: support debian and fedora
    This should be enough to move it out of staging.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- staging/update.sh
+++ staging/update.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get autoclean
-apt-get autoremove
+
+if [ -f /etc/fedora-release ]; then
+  dnf update
+  dnf upgrade -y
+  dnf clean
+  dnf autoremove
+elif [ -f /etc/debian-release ]; then
+  apt-get update
+  apt-get upgrade -y
+  apt-get dist-upgrade -y
+  apt-get autoclean
+  apt-get autoremove
+else
+  echo "fatal: unsupported system"
+fi
