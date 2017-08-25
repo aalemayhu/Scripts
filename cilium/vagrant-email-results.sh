79dd6a93bdce80d1704a03d7990668fd2be9dba2
Alexander Alemayhu <alexander@alemayhu.com>
2017-08-25 19:00:00 +0200

    cilium: make it easier to cilium/master for vagrant email build
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- cilium/vagrant-email-results.sh
+++ cilium/vagrant-email-results.sh
@@ -2,11 +2,12 @@
 
 export VAGRANT_LOG=debug
 
-EMAIL="`mktemp`.build"
+EMAIL="`mktemp`.email.txt"
 REV="$(git --no-pager log -1 --pretty='tformat:%h (%s, %ad)' --date=short)"
 SUBJECT="[BUILD$2]: [cilium]: $REV"
 DATE="`date`"
 FROM="Alexander Alemayhu <alexander@alemayhu.com>"
+REPO=""
 
 echo "From: $FROM" > $EMAIL
 echo "Date: $DATE" >> $EMAIL
@@ -16,7 +17,21 @@ echo "PWD=`pwd`" >> $EMAIL
 echo "" >> $EMAIL
 echo "" >> $EMAIL
 
+if [[ "$1" == "clone" ]]; then
+  REPO=`mktemp`
+  rm $REPO && mkdir $REPO
+  git clone https://github.com/cilium/cilium $REPO >> $EMAIL 2>&1
+  # TODO: check for a local cache, and git fetch then copy.
+  cd $REPO
+fi
+
 RELOAD=1 ./contrib/vagrant/start.sh >> $EMAIL 2>&1
 RUN_TEST_SUITE=1 ./contrib/vagrant/start.sh >> $EMAIL 2>&1
+
+if [[ "$3" == "cleanup" ]]; then
+  vagrant destroy -f >> $EMAIL 2>&1
+  cd -
+  rm -rvf $REPO >> $EMAIL 2>&1
+fi
 
 git send-email --no-validate --to alexander@covalent.io --suppress-cc=all $EMAIL
