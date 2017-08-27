11aedb14ebd01a6ecfcf42ebecfa731c4ef9b173
Alexander Alemayhu <alexander@alemayhu.com>
2017-08-27 15:34:22 +0200

    cilium: move clone part further up
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- cilium/vagrant-email-results.sh
+++ cilium/vagrant-email-results.sh
@@ -2,12 +2,21 @@
 
 export VAGRANT_LOG=debug
 
+REPO=""
+
+if [[ "$1" == "clone" ]]; then
+  REPO=`mktemp`
+  rm $REPO && mkdir $REPO
+  git clone https://github.com/cilium/cilium $REPO
+  # TODO: check for a local cache, and git fetch then copy.
+  cd $REPO
+fi
+
 EMAIL="`mktemp`.email.txt"
 REV="$(git --no-pager log -1 --pretty='tformat:%h (%s, %ad)' --date=short)"
 SUBJECT="[BUILD$2]: [cilium]: $REV"
 DATE="`date`"
 FROM="Alexander Alemayhu <alexander@alemayhu.com>"
-REPO=""
 
 echo "From: $FROM" > $EMAIL
 echo "Date: $DATE" >> $EMAIL
@@ -16,14 +25,6 @@ echo "Subject: $SUBJECT" >> $EMAIL
 echo "PWD=`pwd`" >> $EMAIL
 echo "" >> $EMAIL
 echo "" >> $EMAIL
-
-if [[ "$1" == "clone" ]]; then
-  REPO=`mktemp`
-  rm $REPO && mkdir $REPO
-  git clone https://github.com/cilium/cilium $REPO >> $EMAIL 2>&1
-  # TODO: check for a local cache, and git fetch then copy.
-  cd $REPO
-fi
 
 RELOAD=1 ./contrib/vagrant/start.sh >> $EMAIL 2>&1
 RUN_TEST_SUITE=1 ./contrib/vagrant/start.sh >> $EMAIL 2>&1
