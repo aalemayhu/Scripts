5219a0d474c5b7a8ace4ca50f223eef9f3eef780
Alexander Alemayhu <alexander@alemayhu.com>
2019-05-07 22:17:47 +0200

    update: deno fmt
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- update.ts
+++ update.ts
@@ -2,37 +2,36 @@
 import { exists } from "https://deno.land/std/fs/mod.ts";
 
 async function main() {
-  const isFedora = await exists('/etc/fedora-release');
+  const isFedora = await exists("/etc/fedora-release");
   if (isFedora) {
-    const commands = ['update', 'upgrade', 'clean packages', 'autoremove'];
+    const commands = ["update", "upgrade", "clean packages", "autoremove"];
     for (const cmd of commands) {
-      await Deno.run({args: ['dnf', '-y', cmd]});
+      await Deno.run({ args: ["dnf", "-y", cmd] });
     }
     return;
   }
 
-  const isDebian = await exists('/etc/debian-release');
-  let hasAptGet = isDebian
+  const isDebian = await exists("/etc/debian-release");
+  let hasAptGet = isDebian;
 
   if (!isDebian) {
     try {
-      await Deno.run({args: ['apt-get', '--version']});
+      await Deno.run({ args: ["apt-get", "--version"] });
       hasAptGet = true;
     } catch (error) {}
   }
 
   if (isDebian || hasAptGet) {
-    await Deno.run({ args: ['apt-get', 'update'] });
-    await Deno.run({ args: ['apt-get', 'upgrade', '-y']});
-    await Deno.run({ args: ['apt-get', 'upgrade', '-y']});
-    await Deno.run({ args: ['apt-get', 'autoclean'] });
-    await Deno.run({ args: ['apt-get', 'autoremove'] });
+    await Deno.run({ args: ["apt-get", "update"] });
+    await Deno.run({ args: ["apt-get", "upgrade", "-y"] });
+    await Deno.run({ args: ["apt-get", "upgrade", "-y"] });
+    await Deno.run({ args: ["apt-get", "autoclean"] });
+    await Deno.run({ args: ["apt-get", "autoremove"] });
     return;
   }
 
-  console.error('fatal: unsupported system');
+  console.error("fatal: unsupported system");
   Deno.exit(1);
 }
 
-
-main()
+main();
